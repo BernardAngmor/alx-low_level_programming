8d37e6234cc370164674f57ba307ba8334c3253b
BernardAngmor <bernardangmor98@gmail.com>
2023-03-02 08:58:40 -0800

    My very own work

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - execution and termination of program
+ *
+ *Return: ends main function
+ */
+int main(void)
+{
+	char str_1[] = "and that piece of art is useful";
+	char name[] = "Dora Korpar";
+	int year = 2015;
+	int month = 10;
+	int day = 19;
+
+	fprintf(stderr, "%s\" - %s, %d-%d-%d\n", str_1, name, year, month, day);
+	return (1);
+}
